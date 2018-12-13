6280112c390fe816fd0af47c3a94501dfbc19963
Isaac Fonseca <isaacfo1010@gmail.com>
2018-12-13 10:29:58 -0500

    added comments and set action update

---
+++ index.js
@@ -0,0 +1,10 @@
+
+
+
+// ==========================================================
+// APP
+// ==========================================================
+
+
+// #6- set up subsciptions for notifications of when the state changes 
+// and perform changes to the state
---
+++ store.js
@@ -0,0 +1,50 @@
+const {createStore} = require('redux')
+
+
+
+
+
+// ==========================================================
+// SETUP
+// ==========================================================
+// #1- Describe the state
+const defaultState = {
+    note: {
+        content:''
+    }
+}
+
+
+// #2- List out all the changes you could make to state
+/*
+- UPDATE_CONTENT
+*/
+const ACTION_UPDATE = {
+    type: 'ACTION_UPDATE'
+}
+
+
+// #3- Wrrite action creator functions
+// These are fucntions that use/return those descriptions
+// of changes you can make to state
+const updateContent = (content) => {
+    return {
+        ...ACTION_UPDATE,
+        content
+    }
+}
+
+
+// #4- Write a reducer fucntion that accepts state and an action
+// and returns the next version of the state
+const note = (state, action) => {
+    switch(action.type) {
+        case ACTION_UPDATE.type:
+        // if so do stuff
+        defualt:
+            return state;
+    }
+}
+
+
+// $5- Creat a store that uses your reducer
