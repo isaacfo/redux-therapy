83c7e4e1956ecc4382ba94ce1f1eb42d45dcdb77
Isaac Fonseca <isaacfo1010@gmail.com>
2018-12-13 11:44:40 -0500

    added more action and creators

--- index.js
+++ index.js
@@ -1,4 +1,7 @@
-
+const {
+    store,
+    updateContent
+} = require('./store');
 
 
 // ==========================================================
@@ -8,3 +11,14 @@
 
 // #6- set up subsciptions for notifications of when the state changes 
 // and perform changes to the state
+store.subscribe(() => {
+    console.log('-------------');
+    console.log(store.getState());
+    console.log('-------------')
+});
+
+store.dispatch(updateContent('buy some milk'));
+store.dispatch(updateContent('drink the milk'));
+store.dispatch(updateContent('buy some cereal'));
+store.dispatch(updateContent('eat the cereal'));
+store.dispatch(updateContent('uu la la'));``
--- store.js
+++ store.js
@@ -1,5 +1,5 @@
-const {createStore} = require('redux')
-
+const {createStore} = require('redux');
+const uuid = require('uuid/v4');
 
 
 
@@ -9,30 +9,63 @@ const {createStore} = require('redux')
 // ==========================================================
 // #1- Describe the state
 const defaultState = {
-    note: {
-        content:''
-    }
+    notes: [
+
+        {
+            id: uuid(),
+            content:''
+        }
+    ]
 }
 
 
 // #2- List out all the changes you could make to state
 /*
 - UPDATE_CONTENT
+- ADD_NOTE
+- DELETE_NOTE
 */
 const ACTION_UPDATE = {
     type: 'ACTION_UPDATE'
-}
+    // content: '?????'
+    // id: '??????'
+};
+
+const ACTION_ADD = {
+    type: 'ACTION_ADD',
+    //content: '???'
+};
+
+const ACTION_DEL = {
+    type: 'ACTION_DEL',
+    // id: '???'
+};
 
 
-// #3- Wrrite action creator functions
+// #3- Write action creator functions
 // These are fucntions that use/return those descriptions
 // of changes you can make to state
-const updateContent = (content) => {
+const updateContent = (id, content) => {
     return {
         ...ACTION_UPDATE,
+        id,
         content
     }
-}
+};
+
+const addNote = (content) => {
+    return {
+        ...ACTION_ADD,
+        content
+    }
+};
+
+const deleteNote = (content) => {
+    return {
+        ...ACTION_DEL,
+        id
+    }
+};
 
 
 // #4- Write a reducer fucntion that accepts state and an action
@@ -42,9 +75,43 @@ const note = (state, action) => {
         case ACTION_UPDATE.type:
         // if so do stuff
             return {
+                notes: state.notes.map(note => {
+                    if (note.id=== action.id) {
+                        return {
+                             ...note,
+                             // alternative to spread operator: just copy all the exisiting keys 
+                            //  id: note.id,
+                            //  content: note.content,
+
+                            // immeditaley overwrite the content with what
+                            // came in throuh the action 
+                            content: action.content
+                        }
+                    
+                    }else {
+                        // this is not the on to update
+                        // just return the original
+                        // return note:
+
+                        // Anal retentive
+                    }
+                })
                 note: {
                     content: action.content
                 }
+            }
+        
+        case ACTION_ADD.type:
+            return {
+                notes: [
+                    // bringing in the previous content in the state
+                    ...state.notes,
+                    { //defining new id for the result of the action 
+                        id: uuid(),
+                        // the content created from the action 
+                        content: action.content
+                    }
+                ]
             }
         default:
             return state;
